317682c2a284d1f8afadc27cdfaf883cb88f64e7
Jason YTH <jasonyth0818@gmail.com>
2019-12-01 00:45:47 -0500

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -114,8 +114,8 @@ async function train(model, data) {
   const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
   
   const BATCH_SIZE = 512;
-  const TRAIN_DATA_SIZE = 5500;
-  const TEST_DATA_SIZE = 1000;
+  const TRAIN_DATA_SIZE = 3000;
+  const TEST_DATA_SIZE = 3000;
 
   const [trainXs, trainYs] = tf.tidy(() => {
     const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
